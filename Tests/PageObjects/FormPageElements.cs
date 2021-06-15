312c25686a776998a7b7d350a37461329480e470
QMallon <57999351+QMallon@users.noreply.github.com>
2021-06-15 16:00:51 -0400

    BasicStuff

---
+++ Tests/PageObjects/FormPageElements.cs
@@ -0,0 +1,76 @@
+﻿using OpenQA.Selenium;
+//using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+
+
+namespace Tests
+{
+    class FormPageElements
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [FindsBy(How = How.Id, Using = ("inputEmail4"))]
+        private IWebElement emailBox;
+
+        [FindsBy(How = How.Id, Using = ("inputPassword4"))]
+        private IWebElement passwordBox;
+
+        [FindsBy(How = How.Id, Using = ("inputAddress"))]
+        private IWebElement addressBox1;
+
+        [FindsBy(How = How.Id, Using = ("inputCity"))]
+        private IWebElement cityBox;
+
+        [FindsBy(How = How.Id, Using = ("inputZip"))]
+        private IWebElement zipBox;
+
+        [FindsBy(How = How.Id, Using = ("gridCheck"))]
+        private IWebElement checkBox;
+
+
+        public FormPageElements(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            PageFactory.InitElements(driver, this);
+
+        }
+
+        public void setEmail(String value)
+        {
+            this.emailBox.SendKeys(value);
+        }
+
+        public void setPassword(String value)
+        {
+            this.passwordBox.SendKeys(value);
+        }
+
+        public void setAddress(String value)
+        {
+            this.addressBox1.SendKeys(value);
+        }
+
+        public void setCity(String value){
+            this.cityBox.SendKeys(value);
+        }
+
+        public void setZip(String value)
+        {
+            this.zipBox.SendKeys(value);
+        }
+        public void setcheckBox()
+        {
+            checkBox.Click();
+        }
+
+    }
+}
---
+++ Tests/TestWebAppTests.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using SeleniumExtras.PageObjects;
+
+namespace Tests
+{
+    public class TestWebAppTests
+    {
+
+        private FormPageElements formPage;
+
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadAboutPage()
+        { 
+            using(IWebDriver driver = new ChromeDriver())
+            {
+                formPage = new FormPageElements(driver);
+                driver.Navigate().GoToUrl("https://localhost:44310/");
+                String title = driver.Title;
+                Assert.Equal("Home page - TestWebApp", title);
+            }
+            
+        }
+
+        [Fact]
+        public void LoadFormPage()
+        
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:44310/form");
+                //driver.FindElement(By.XPath("/ html / body / nav / div / div[2] / ul / li[4] / a")).Click();
+                String title = driver.Title;
+                Assert.Equal("FormTest - TestWebApp", title);
+
+            }
+        }
+        [Fact]
+        public void submitFormBlank()
+        {
+            using (IWebDriver driver = new ChromeDriver()) 
+            {
+                driver.Navigate().GoToUrl("https://localhost:44310/form");
+                driver.FindElement(By.Id("btnSubmit")).Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Assert.Equal("Email is blank", alert.Text);
+
+            }
+        }
+        [Fact]
+        public void submitFormBlankPassword()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                formPage = new FormPageElements(driver);
+                driver.Navigate().GoToUrl("https://localhost:44310/form");
+                formPage.setEmail("quinn@email.com");
+                
+                driver.FindElement(By.Id("btnSubmit")).Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Assert.Equal("password is blank", alert.Text);
+
+            }
+        }
+
+        [Fact]
+        public void submitFormBlankAddress()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:44310/form");
+                formPage = new FormPageElements(driver);
+               
+                formPage.setEmail("quinn@email.com");
+                formPage.setPassword("1234");
+                driver.FindElement(By.Id("btnSubmit")).Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Assert.Equal("address is blank", alert.Text);
+
+            }
+        }
+
+        [Fact]
+        public void submitFormBlankCity()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:44310/form");
+                formPage = new FormPageElements(driver);
+
+                formPage.setEmail("quinn@email.com");
+                formPage.setPassword("1234");
+                formPage.setAddress("1234 Street");
+                
+                driver.FindElement(By.Id("btnSubmit")).Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Assert.Equal("city is blank", alert.Text);
+            }
+        }
+
+        [Fact]
+        public void submitFormBlankZip()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:44310/form");
+                formPage = new FormPageElements(driver);
+
+                formPage.setEmail("quinn@email.com");
+                formPage.setPassword("1234");
+                formPage.setAddress("1234 Street");
+                formPage.setCity("City");
+                driver.FindElement(By.Id("btnSubmit")).Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Assert.Equal("zip is blank", alert.Text);
+            }
+        }
+
+        [Fact]
+        public void submitFormBlankCheck()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:44310/form");
+                formPage = new FormPageElements(driver);
+
+                formPage.setEmail("quinn@email.com");
+                formPage.setPassword("1234");
+                formPage.setAddress("1234 Street");
+                formPage.setCity("City");
+                formPage.setZip("12345");
+
+                driver.FindElement(By.Id("btnSubmit")).Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Assert.Equal("checkbox is blank", alert.Text);
+            }
+        }
+
+        [Fact]
+        public void SubmitFormSuccess()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:44310/form");
+                formPage = new FormPageElements(driver);
+
+                formPage.setEmail("quinn@email.com");
+                formPage.setPassword("1234");
+                formPage.setAddress("1234 Street");
+                formPage.setCity("City");
+                formPage.setZip("12345");
+                formPage.setcheckBox();
+
+                driver.FindElement(By.Id("btnSubmit")).Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Assert.Equal("Submited Form", alert.Text);
+            }
+        }
+
+    }
+}
